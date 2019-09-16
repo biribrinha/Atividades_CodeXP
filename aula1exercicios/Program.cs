beb7c410d97f70d23f6ddd0c32a877c7bcb6cd87
juliabergman <juliabergman523@gmail.com>
2019-09-16 12:28:31 -0300

    adicionada segunda estrutura de decisão

---
+++ aula1exercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aula1exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,m;
+            Console.WriteLine("digte um numero");
+            num=int.Parse(Console.ReadLine());
+            m=num/3;
+            Console.WriteLine(m);
+        }
+    }
+}
---
+++ aula2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace aula2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definimos as variaveis que utilizaremos para fazer o cálculo//
+            float nota1, nota2, nota3, media;
+            string resultado;
+
+            Console.Write ("digite a primeira nota:");
+            nota1 = float.Parse ( Console.ReadLine());
+
+            
+            Console.Write ("digite a segunda nota:");
+            nota2 = float.Parse ( Console.ReadLine());
+
+            
+            Console.Write ("digite a terceira nota:");
+            nota3 = float.Parse ( Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) /3;
+            if(media>=6){
+                resultado = "aprovado";
+            }else if(media < 6 && media >= 4){
+
+                resultado = "recuperação";
+            }else{
+                resultado = "reprovado";
+            }
+
+            Console.WriteLine($"Média: {media} - Aluno está de {resultado}");
+        }
+    }
+}
