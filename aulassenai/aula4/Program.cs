9725c2557ad04b41f2558fdafbe77b2206d6d87f
juliabergman <juliabergman523@gmail.com>
2019-09-20 12:29:58 -0300

    aula 4-vetor

---
+++ aulassenai/aula4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] media = new double[10];
+
+            // o laço tem que começar do 0 pq variável 
+            //funciona assim
+            for (int i = 0; i <= 9; i++)
+            {
+
+                Console.Write("digite uma media: ");
+                media[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(media[3]);
+            Console.WriteLine();
+                /// o lenght pega a quantidade de vetores 
+            ///   que eu tenho (caso eu não soubesse a quantidade)
+            for(int i=0; i <= media.Length -1 ; i ++){
+
+                Console.WriteLine("media : "+ media[i] );
+            }
+        }
+    }
+}
